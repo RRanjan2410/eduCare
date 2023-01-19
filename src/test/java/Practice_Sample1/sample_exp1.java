65043a35d6a47e4fe0e63746f052a938b3e8994a
ashir <ashir@LAPTOP-HK26NAV8>
2023-01-19 12:18:04 +0530

    comitted

---
+++ src/test/java/Practice_Sample1/sample_exp1.java
@@ -0,0 +1,12 @@
+package Practice_Sample1;
+
+import org.testng.annotations.Test;
+
+public class sample_exp1 {
+	
+	@Test
+	public void sampleexp2() {
+		System.out.println("my name is ashir");
+	}
+
+}

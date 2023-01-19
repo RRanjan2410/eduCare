a8c4baddde7224dd059eba0c4619050a7c4a8fb3
ashir <ashir@LAPTOP-HK26NAV8>
2023-01-19 12:58:04 +0530

    done

--- src/test/java/Practice_sample2/sample_exp2.java
+++ src/test/java/Practice_sample2/sample_exp2.java
@@ -1,5 +1,10 @@
 package Practice_sample2;
 
-public class sample_exp2 {
+import org.testng.annotations.Test;
 
+public class sample_exp2 {
+@Test
+public void sample23() {
+	System.out.println("the id is generated");
+}
 }
